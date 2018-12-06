8c922828bf45ecf0499f6b6bf771b48d89960bfd
Clack321 <dajohnson1998@gmail.com>
2018-12-06 16:56:55 -0500

    completed shopping list functionality

--- shoppinglist/index.js
+++ shoppinglist/index.js
@@ -1,27 +1,3 @@
-/*(document).ready(function(){
-
-  // get text entered into field via button or return
-// when check button is clicked,  change class of list item to strikethrough
-// when delete button is clicked, remove item permanently
-
-$('.js-shopping-list-form').submit( function(event) {
-  console.log(event);
-  const outputVar = $('.js-shopping-list-form').val
-  console.log(outputVar);
-  event.preventDefault();
-})
-// .submit() //when form is returned and pass in function (event handler)
-// .preventDefault( ) default action not triggered ( for form submission)
-// .toggleClass() add or remove 1+ classes from each element in set of matched elements
-// depending on either class presence or value  (name) or (name, state) (({check off list button)))
-// .closest(selector) for each element in set get first element that matches the selector by testing
-//  the element itself and traversing up through ancestors in dom tree.
-
-
-
-});*/
-
-
 function getTextFromForm() {
   console.log('register listener')
   $('#js-shopping-list-form').submit(function(event) {
@@ -32,18 +8,15 @@ function getTextFromForm() {
   $('.shopping-list').append(HTML);
   })
 };
+
 function isCheckButtonClicked() {
   $('.shopping-list').on('click', '.shopping-item-toggle', function(){
     const jObject = $(this).closest('li').find('.shopping-item') //.remove for delete
-    jObject.toggleClass('.shopping-item__checked');
-
+    //console.log(jObject);
+    jObject.hasClass("shopping-item__checked") ? jObject.removeClass("shopping-item__checked") : jObject.addClass("shopping-item__checked");
+    //Used tenerary operator in place of toggleClass, becuase toggleClass was not functioning.
   })
 }
-
-// function changeClassOfList(jObject) {
-//   jObject.toggleClass('.shopping-item__checked');
-  
-// }
 
 function entryToString(newListItem){
   return ` <li>
@@ -60,14 +33,16 @@ function entryToString(newListItem){
 }
 
 function deletebutton() {
-
+  $('.shopping-list').on('click', '.shopping-item-delete', function(){
+     $(this).closest('li').remove();
+  })
 }
+
 function main() {
-
-isCheckButtonClicked();
-
-getTextFromForm();
-
+  isCheckButtonClicked();
+  getTextFromForm();
+  deletebutton();
 }
+
 $(main);
 
