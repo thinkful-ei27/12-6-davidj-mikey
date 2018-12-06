ddc2b2f15abac550b640c62dfbd53e21cb014951
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-06 14:52:17 -0500

    really the set for shopping list

---
+++ shoppinglist/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  // get text entered into field via button or return
+// when check button is clicked,  change class of list item to strikethrough
+// when delete button is clicked, remove item permanently
+
+// .submit() //when form is returned and pass in function (event handler)
+// .preventDefault( ) default action not triggered ( for form submission)
+// .toggleClass() add or remove 1+ classes from each element in set of matched elements
+// depending on either class presence or value  (name) or (name, state) (({check off list button)))
+// .closest(selector) for each element in set get first element that matches the selector by testing
+//  the element itself and traversing up through ancestors in dom tree.
+
+
+
+});
