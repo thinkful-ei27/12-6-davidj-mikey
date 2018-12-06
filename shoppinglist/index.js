ccf8888842336f9c65313773174c7b1f7397eac5
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-06 16:36:58 -0500

    almost ther , just toggle buttons

--- shoppinglist/index.js
+++ shoppinglist/index.js
@@ -20,27 +20,54 @@ $('.js-shopping-list-form').submit( function(event) {
 
 
 });*/
+
+
 function getTextFromForm() {
-  $('.js-shopping-list-form').submit(function(event) {
+  console.log('register listener')
+  $('#js-shopping-list-form').submit(function(event) {
     event.preventDefault();
-    const outputVar = $('.js-shopping-list-form');
+    const outputVar = $('.js-shopping-list-entry');
     const output = outputVar.val();
-    console.log(output);
+  const HTML = entryToString(output);
+  $('.shopping-list').append(HTML);
   })
 };
 function isCheckButtonClicked() {
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(){
+    const jObject = $(this).closest('li').find('.shopping-item') //.remove for delete
+    jObject.toggleClass('.shopping-item__checked');
 
+  })
 }
 
-function changeClassOfList() {
-  //change list to strikethrough
+// function changeClassOfList(jObject) {
+//   jObject.toggleClass('.shopping-item__checked');
+  
+// }
+
+function entryToString(newListItem){
+  return ` <li>
+  <span class="shopping-item">${newListItem}</span>
+  <div class="shopping-item-controls">
+    <button class="shopping-item-toggle">
+      <span class="button-label">check</span>
+    </button>
+    <button class="shopping-item-delete">
+      <span class="button-label">delete</span>
+    </button>
+  </div>
+</li> `
 }
 
 function deletebutton() {
 
 }
-
 function main() {
+
+isCheckButtonClicked();
+
+getTextFromForm();
 
 }
 $(main);
+
