5cdbe30b0f372930dd25c9b23afb313c7d9d2e98
Clack321 <dajohnson1998@gmail.com>
2018-12-06 15:24:40 -0500

    unfinished shopping app

--- shoppinglist/index.js
+++ shoppinglist/index.js
@@ -1,9 +1,15 @@
-$(document).ready(function(){
+/*(document).ready(function(){
 
   // get text entered into field via button or return
 // when check button is clicked,  change class of list item to strikethrough
 // when delete button is clicked, remove item permanently
 
+$('.js-shopping-list-form').submit( function(event) {
+  console.log(event);
+  const outputVar = $('.js-shopping-list-form').val
+  console.log(outputVar);
+  event.preventDefault();
+})
 // .submit() //when form is returned and pass in function (event handler)
 // .preventDefault( ) default action not triggered ( for form submission)
 // .toggleClass() add or remove 1+ classes from each element in set of matched elements
@@ -13,4 +19,13 @@ $(document).ready(function(){
 
 
 
-});
+});*/
+function main() {
+  $('.js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    const outputVar = $('.js-shopping-list-form');
+    const output = outputVar.val();
+    console.log(output);
+  })
+};
+$(main);
